cbd73992dd2610746356545c8d0995e677ba261c
YokotaYusuke <tarut8166@gmail.com>
2023-09-25 15:20:01 +0900

    sample.jsとhelper.jsを作成

---
+++ sample/helper.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+const person = {
+  name: "Mike",
+  age: 21,
+  country: "japan"
+}
+
+const read = function() {
+  fs.readFile("./sample.json","utf8", function(err, data) {
+    const person = JSON.parse(data);
+    console.log(person.country, person.age);
+  });
+}
+
+const write = function() {
+  fs.writeFile("./sample.json", JSON.stringify(person), function() {
+    console.log("書き出し完了");
+  });
+}
+
+module.exports = {
+  read: read,
+  write: write
+}
